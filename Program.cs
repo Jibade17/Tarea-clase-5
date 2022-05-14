519b696039f99824507c9a8761941c49797e09bc
Nahuel Agustin Alberti <105518230+Jibade17@users.noreply.github.com>
2022-05-14 14:25:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+var respuesta = "S";
+
+while (respuesta.ToUpper() ==  "S")
+{
+    Console.WriteLine("Ingrese su nombre");
+    var Nombre = Console.ReadLine();
+    Console.WriteLine("Hola " + Nombre);
+    Console.WriteLine("Desea continuar S/N?");
+    respuesta = Console.ReadLine();
+}
+
+if (respuesta.ToUpper() == "N")
+{
+    Console.WriteLine("Programa finalizado correctamente");
+}
+else
+{
+    Console.WriteLine("Opción no válida");
+}
